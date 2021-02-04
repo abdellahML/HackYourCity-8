689539eba307b65938ba2d31a2a8962cd060b8ed
Ebu <ebu@iBoo.lan>
2021-02-04 17:39:22 +0100

    little change

--- app/data_collecting.py
+++ app/data_collecting.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random
+import numpy as np
 
 class dataCollecting:
 
@@ -57,5 +58,25 @@ class dataCollecting:
         #We couldn't make it for this hackathon because we hadn't enough data and especially good data
         pass
 
-    def choose_a_place(self, df):
-        """This function will choose place according to criteria, but since for now we didn't have enough response"""
+    def choose_a_place(self, df, persona):
+        """This function will choose place according to criteria, but since for now we didn't have enough response
+        we make a mock to be able to finish our project. This function will give point according to the persona
+        and will put in descending order"""
+
+        score = np.array([[5, 0, 3, 3, 3], 
+                          [3, 5, 4, 4, 1],
+                          [2, 3, 5, 4, 4],
+                          [3, 3, 3, 5, 3],
+                          [3, 1, 3, 4, 5]])
+
+        if persona == "famille":
+            if df['type'] == "famille":
+                df['score'] = 5
+            elif df["type"] == "solo":
+                df['score'] = 0
+            elif df['type'] == "friends":
+                df['score'] = 3
+            elif df['type'] == "tourist":
+                df['score'] = 3
+            elif df['type'] == "couple":
+                df['score'] = 3
