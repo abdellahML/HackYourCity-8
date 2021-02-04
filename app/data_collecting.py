f40308f318d025c44ff06a45c5b4e7bf42d2cb45
Ebu <ebu@iBoo.lan>
2021-02-04 14:34:43 +0100

    finish return activities places function

--- app/data_collecting.py
+++ app/data_collecting.py
@@ -8,34 +8,54 @@ class dataCollecting:
         if what == "sport":
             if location == "interior":
                 if animation == "foule":
+
                     print("Rien pour le moment, on sait qu'il y a le laser game")
                 
                 elif animation == "solo":
-                    print("Rien pour le moment")
+
+                    print("Rien pour le moment, salle de fitness à ajouter")
             
             elif location =="exterior":
                 if animation == "foule":
+
                     df = pd.read_csv("assets/festivMF.csv")
                 
                 elif animation == "solo":
-                    df = pd.read_csv("Data/hannut_centre_equestre.csv")
+
+                    files["Data/hannut_centre_equestre.csv", '"Data/marche_pt_vert.csv"']
+                    i = random.randint(len(files))
+                    df = pd.read_csv(files[i])
         
         elif what == "musee":
             if location == "interior":
                 if animation == "foule":
+
                     files['assets/brasserieMF.csv', 'assets/cinemaMF.csv']
                     i = random.randint(len(files))
                     df = pd.read_csv(files[i])
                 
                 elif animation == "solo":
-                    df = pd.read_csv('assets/museesMF.csv')
+
+                    files['assets/museesMF.csv', 'Data/hannut_femme_detente.csv']
+                    i = random.randint(len(files))
+                    df = pd.read_csv(files[i])
             
             elif location =="exterior":
                 if animation == "foule":
-                    print('rien pour le moment')
+                    print('rien pour le moment, on pourra mettre les marchés, evenement culturelle, marché artisanal')
                 
                 elif animation == "solo":
                     df = pd.read_csv('assets/patrimonesMF.csv')
+        return df
+
+
     
     def create_list_csv_by_city(self, file_name, city_name):
-        """This function will """
+        """This function will sort the database according to the city, so it will take only places which are in the city
+        and return it"""
+
+        #We couldn't make it for this hackathon because we hadn't enough data and especially good data
+        pass
+
+    def choose_a_place(self, df):
+        """This function will choose place according to criteria, but since for now we didn't have enough response"""
